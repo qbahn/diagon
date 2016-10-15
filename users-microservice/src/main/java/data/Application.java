c0cf00cf4fe66bad747d66351160f0815bf6f35a
Wojtek Pietrucha <wojtekpietrucha@gmail.com>
2016-10-16 00:54:30 +0200

    Launched 3 microservices.

--- users-microservice/src/main/java/data/Application.java
+++ users-microservice/src/main/java/data/Application.java
@@ -6,6 +6,7 @@ import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -16,6 +17,7 @@ import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @SpringBootApplication
 @EnableNeo4jRepositories
+@EnableDiscoveryClient
 @EnableZuulProxy
 @EnableHystrix
 public class Application extends Neo4jConfiguration {
